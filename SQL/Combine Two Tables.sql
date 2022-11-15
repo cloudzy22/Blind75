2061ba4ea1084c67248bbfb301661c45ee4a3692
michal <116053839+cloudzy22@users.noreply.github.com>
2022-11-15 06:24:30 +0100

    Create Combine Two Tables.sql

---
+++ SQL/Combine Two Tables.sql
@@ -0,0 +1,24 @@
+/*Input: 
+Person table:
++----------+----------+-----------+
+| personId | lastName | firstName |
++----------+----------+-----------+
+| 1        | Wang     | Allen     |
+| 2        | Alice    | Bob       |
++----------+----------+-----------+
+Address table:
++-----------+----------+---------------+------------+
+| addressId | personId | city          | state      |
++-----------+----------+---------------+------------+
+| 1         | 2        | New York City | New York   |
+| 2         | 3        | Leetcode      | California |
++-----------+----------+---------------+------------+
+Output: 
++-----------+----------+---------------+----------+
+| firstName | lastName | city          | state    |
++-----------+----------+---------------+----------+
+| Allen     | Wang     | Null          | Null     |
+| Bob       | Alice    | New York City | New York |
++-----------+----------+---------------+----------+ */
+
+SELECT Person.firstName, Person.lastName, Address.City, Address.State FROM Person LEFT JOIN Address ON Address.personId = Person.personId
